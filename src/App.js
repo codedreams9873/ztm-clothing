d24e47b830047426f85518494cacff8ae311bac0
codedreams <codedreams9873@gmail.com>
2024-04-12 11:17:05 -0700

    Step1. implemented basci routing.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,48 @@
+<<<<<<< HEAD
+const App = () => {
+  return (
+    <div className="App">
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Women</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Men</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+=======
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
@@ -16,5 +61,6 @@ const App = () => {
 		</Routes>
 	);
 };
+>>>>>>> b63054b (Step1. implemented basci routing.)
 
 export default App;
