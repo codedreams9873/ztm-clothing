d121b75bf4c9c7a531738c742d073c18368af99d
codedreams <codedreams9873@gmail.com>
2024-04-12 00:14:58 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+const App = () => {
+  return (
+    <div className="App">
+      <div className="categories-container">
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Women</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        <div className="category-container">
+          {/* <img src="" /> */}
+          <div className="category-body-container">
+            <h2>Men</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
