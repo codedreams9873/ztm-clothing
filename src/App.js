f667c595b7c0fd8f452af77714e5bc28579f4298
codedreams <codedreams9873@gmail.com>
2024-04-13 03:17:37 -0700

    setup firebase sign in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
+import SignIn from "./components/auth/sign-in.component";
 
 const Shop = () => {
 	return <h1>This is Shop page.</h1>;
@@ -12,6 +13,7 @@ const App = () => {
 			<Route path="/" element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path="shop" element={<Shop />} />
+				<Route path="signin" element={<SignIn />} />
 			</Route>
 		</Routes>
 	);
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBcPbMERQ5nXs2N8wWB6K_-2oK8E43paek",
+	authDomain: "ztm-clothing-db-1d803.firebaseapp.com",
+	projectId: "ztm-clothing-db-1d803",
+	storageBucket: "ztm-clothing-db-1d803.appspot.com",
+	messagingSenderId: "288620041980",
+	appId: "1:288620041980:web:07d7b449dee3994982e979",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+	prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createDocFromAuth = async (userAuth) => {
+	const userDocRef = doc(db, "user", userAuth.uid);
+	const userSnapshot = await getDoc(userDocRef);
+
+	// if user does not exists, register
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt,
+			});
+		} catch (error) {
+			console.log("error creating the user", error.message);
+		}
+	}
+
+	return userDocRef;
+};
